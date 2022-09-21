f2b61a7fa1adc218d06aa7d1cc14dcd774962b07
kethnal <kaseyvaldez@hotmail.com>
2022-09-20 19:25:55 -0700

    make assert equal function for arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,11 @@
+const assertArraysEqual = function(array1, array2) {
+  let result = `✅✅✅ Assertion Passed: ${array1} === ${array2}`;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      result = `🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`;
+    }
+  }
+  console.log(result);
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
