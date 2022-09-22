e728694e72eb2ec97983ccc8976596dd154f0707
kethnal <kaseyvaldez@hotmail.com>
2022-09-21 20:30:52 -0700

    make countLetters function

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let output = {};
+  const input = sentence.split(" ").join("");
+  for (let char of input) {
+    if (output[char]) {
+      output[char] += 1;
+    } else {
+      output[char] = 1;
+    }
+  }
+  console.log(output);
+  return output;
+};
+
+const result = countLetters("hello hi");
+
+assertEqual(result["h"], 2);
+assertEqual(result["e"], 1);
+assertEqual(result["l"], 2);
+assertEqual(result["o"], 1);
+assertEqual(result["i"], 1);
