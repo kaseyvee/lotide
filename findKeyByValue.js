05f1ec0b2ed1ea63d2f3f0b7da3c307df9d11864
kethnal <kaseyvaldez@hotmail.com>
2022-09-21 21:20:55 -0700

    make findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (let stuff in object) {
+    if (object[stuff] === value) {
+      return stuff;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
