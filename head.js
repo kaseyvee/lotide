a88ae976934319c75072feafff6a8593e2858596
kethnal <kaseyvaldez@hotmail.com>
2022-09-25 12:32:30 -0700

    added more test cases to head

--- head.js
+++ head.js
@@ -11,4 +11,6 @@ const head = function(array) {
 };
 
 assertEqual(head([5,6,7]), 5);
+assertEqual(head([1]), 1);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([], undefined));
