d335360f15dc109fc0fc5ff56ec21d1af133110b
kethnal <kaseyvaldez@hotmail.com>
2022-09-26 02:34:33 -0700

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -7,14 +7,11 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  let valid = false;
-  if (array1.length === 0 && array2.length === 0) {
-    valid = true;
-  } else if (array1.length === array2.length) {
+  let valid = true;
+
+  if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        valid = true;
-      } else {
+      if (array1[i] !== array2[i]) {
         valid = false;
       }
     }
